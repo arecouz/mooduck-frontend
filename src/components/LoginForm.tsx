3bcf85d26763465d4862332d6b5926b6f8681550
Richard Couzens <richardcouzens96@gmail.com>
2025-05-09 19:52:05 +0100

    login form component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,10 +1,66 @@
-const ContactForm = () => {
+import { useState, ChangeEvent } from 'react';
+
+const LoginForm = () =>  {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Submitted:', formData);
+    // API call to auth, and poss a redirect
+  };
 
   return (
-    <form >
-        <p>form go here</p>
+    <form
+      onSubmit={handleSubmit}
+      className='p-8 border-4 rounded w-full'
+    >
+      <div className='mb-4'>
+        <label htmlFor='email'>
+          Email
+        </label>
+        <input
+          type='email'
+          name='email'
+          id='email'
+          value={formData.email}
+          onChange={handleChange}
+          className='w-full border-3 px-3 py-2 rounded'
+          required
+        />
+      </div>
+      <div className='mb-4'>
+        <label htmlFor='password' className='text-sm font-medium'>
+          Password
+        </label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          value={formData.password}
+          onChange={handleChange}
+          className='w-full border-3 px-3 py-2 rounded'
+          required
+        />
+      </div>
+      <button
+        type='submit'
+        className='w-full border-3 py-2 rounded hover:bg-yellow-100 dark:hover:bg-gray-800'
+      >
+        Login
+      </button>
     </form>
   );
-};
+}
 
-export default ContactForm;
+export default LoginForm;
