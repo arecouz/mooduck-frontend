18dae04fbd9757604b69c14cbf0376b633691248
arecouz <richardcouzens96@gmail.com>
2025-11-21 16:02:52 +0000

    auth re-direct fix

--- src/context/auth/AuthProvider.tsx
+++ src/context/auth/AuthProvider.tsx
@@ -2,11 +2,13 @@ import { ReactNode, useEffect, useState } from 'react';
 import { supabase } from '../../utils/supabaseClient';
 import { Session, User } from '@supabase/supabase-js';
 import { AuthContext } from './AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [session, setSession] = useState<Session | null>(null);
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const loadSession = async () => {
@@ -20,19 +22,28 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         setLoading(false);
       }
     };
+
     loadSession();
 
-    const { data } = supabase.auth.onAuthStateChange((_event, session) => {
-      setTimeout(() => {
-        setSession(session);
-        setUser(session?.user ?? null);
-      });
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+      setUser(session?.user ?? null);
+
+      if (event === 'SIGNED_IN') {
+        window.history.replaceState({}, '', '/dashboard');
+
+        navigate('/dashboard');
+      }
+
+      if (event === 'SIGNED_OUT') {
+        navigate('/login');
+      }
     });
 
     return () => {
       data.subscription.unsubscribe();
     };
-  }, []);
+  }, [navigate]);
 
   return <AuthContext.Provider value={{ user, session, loading }}>{children}</AuthContext.Provider>;
 };
